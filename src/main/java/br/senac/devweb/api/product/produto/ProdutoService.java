dcdac9e39341abef771f42d05d954ccb77e1b5df
hiurydev <81237941+hiurydev@users.noreply.github.com>
2021-10-07 07:58:57 -0300

    alterações aula 07/10

--- src/main/java/br/senac/devweb/api/product/produto/ProdutoService.java
+++ src/main/java/br/senac/devweb/api/product/produto/ProdutoService.java
@@ -1,7 +1,7 @@
 package br.senac.devweb.api.product.produto;
 
-
 import br.senac.devweb.api.product.categoria.Categoria;
+import br.senac.devweb.api.product.categoria.CategoriaRepresentation;
 import br.senac.devweb.api.product.categoria.CategoriaService;
 import br.senac.devweb.api.product.exceptions.NotFoundException;
 import com.querydsl.core.types.Predicate;
@@ -17,10 +17,10 @@ import java.util.List;
 public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
-    private final CategoriaService categoriaService;
 
-    public Produto salvar(ProdutoRepresentation.CreateOrUpdate createOrUpdate) {
-        Categoria categoria = this.categoriaService.getCategoria(createOrUpdate.getCategoria());
+
+    public Produto salvar(ProdutoRepresentation.CreateOrUpdate createOrUpdate,
+                          Categoria categoria) {
 
         Produto produto = Produto.builder()
                 .nome(createOrUpdate.getNome())
@@ -38,25 +38,27 @@ public class ProdutoService {
         return this.produtoRepository.save(produto);
     }
 
-    public Produto atualizar(Long id, ProdutoRepresentation.CreateOrUpdate createOrUpdate) {
-        Produto produtoAntigo = this.buscarUm(id);
-
-        Categoria categoria = this.categoriaService.getCategoria(createOrUpdate.getCategoria());
-
-        Produto produtoAtualizado = produtoAntigo.toBuilder()
-                .nome(createOrUpdate.getNome())
-                .descricao(createOrUpdate.getDescricao())
-                .complemento(Strings.isEmpty(createOrUpdate.getComplemento()) ? "" : createOrUpdate.getComplemento())
-                .fabricante(createOrUpdate.getFabricante())
-                .fornecedor(Strings.isEmpty(createOrUpdate.getFornecedor()) ? "" : createOrUpdate.getFornecedor())
-                .qtde(createOrUpdate.getQtde())
-                .valor(createOrUpdate.getValor())
-                .unidadeMedida(createOrUpdate.getUnidadeMedida())
-                .categoria(categoria)
-                .build();
-
-        return this.produtoRepository.save(produtoAtualizado);
-    }
+//    public Produto atualizar(Long id, ProdutoRepresentation.CreateOrUpdate createOrUpdate) {
+//
+//        Produto produtoAntigo = this.buscarUm(id);
+//
+//        Categoria categoria = this.categoriaService.getCategoria(createOrUpdate.getCategoria());
+//
+//        Produto produtoAtualizado = produtoAntigo.toBuilder()
+//                .nome(createOrUpdate.getNome())
+//                .descricao(createOrUpdate.getDescricao())
+//                .complemento(Strings.isEmpty(createOrUpdate.getComplemento()) ? "" : createOrUpdate.getComplemento())
+//                .fabricante(createOrUpdate.getFabricante())
+//                .fornecedor(Strings.isEmpty(createOrUpdate.getFornecedor()) ? "" : createOrUpdate.getFornecedor())
+//                .qtde(createOrUpdate.getQtde())
+//                .valor(createOrUpdate.getValor())
+//                .unidadeMedida(createOrUpdate.getUnidadeMedida())
+//                .categoria(categoria)
+//                .build();
+//
+//        return this.produtoRepository.save(produtoAtualizado);
+//
+//    }
 
     public List<Produto> buscarTodos(Predicate filter) {
         return this.produtoRepository.findAll(filter);
@@ -74,5 +76,4 @@ public class ProdutoService {
         produto.setStatus(Produto.Status.INATIVO);
         this.produtoRepository.save(produto);
     }
-
 }
